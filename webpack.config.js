8645a044d2d564dbc85d06da9b46b90da8c101b1
martatorredev <hola@martatorre.dev>
2025-03-09 00:22:36 +0100

    Cleaned project - Removed unnecessary files and organized structure

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-
-module.exports = {
-    ...defaultConfig,
-    resolve: {
-        extensions: ['.js', '.jsx']
-    }
-};
